e78cfc2bfdbc788c473f008ab868954e3418fa1d
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-10-27 16:42:36 +0800

    update browserrouter base

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Discord-Clone">
         <React.StrictMode>
             <App />
         </React.StrictMode>
