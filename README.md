af5cd77e7309137bdb985cdc8ba1c0a31f77c300
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-10-27 19:00:20 +0800

    main ui

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A work-in-progress Discord clone!
 
+## Acknowledgement
+
+Massive thanks to @theteachr's codealong for discord's main UI.[Replicating Discord UI](https://www.youtube.com/watch?v=EyB3J7kTc8k&list=PLzqOcheqoAE_ewZ_ypwESF4muN1A6p5XB&index=3)
 
 ## Languages Used
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import './fonts/fonts.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="/Discord-Clone">
+    <HashRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </BrowserRouter>
+    </HashRouter>
 );
